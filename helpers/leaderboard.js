5259e63e7a0552d3241e2be49a1374a7fc787d77
Amit Suthar <sutharamit707@gmail.com>
2024-11-19 21:18:51 +0530

    leaderboard-1

--- helpers/leaderboard.js
+++ helpers/leaderboard.js
@@ -2,17 +2,18 @@ require("dotenv").config();
 const User = require("../models/user");
 
 async function handleLeaderboardCommand(interaction) {
-  // Retrieve users and calculate engagement for each
+  // Retrieve all users and calculate total views for each
   const users = await User.find();
+
   const leaderboard = users
     .map((user) => ({
       username: user.discordName,
-      totalEngagement: user.instagramAccounts.reduce(
-        (acc, account) => acc + account.averageEngagementRate || 0,
+      totalViews: user.reelUrls.reduce(
+        (acc, reel) => acc + (reel.stats.views || 0),
         0
       ),
     }))
-    .sort((a, b) => b.totalEngagement - a.totalEngagement) // Sort by engagement
+    .sort((a, b) => b.totalViews - a.totalViews) // Sort by total views
     .slice(0, 10); // Take top 10
 
   if (leaderboard.length === 0) {
@@ -25,11 +26,13 @@ async function handleLeaderboardCommand(interaction) {
   const leaderboardMessage = leaderboard
     .map(
       (user, index) =>
-        `#${index + 1} **${user.username}** - Engagement: ${
-          user.totalEngagement
-        }`
+        `#${index + 1} **${
+          user.username
+        }** - Views: ${user.totalViews.toLocaleString()}`
     )
     .join("\n");
+
+  // Send the leaderboard message
   await interaction.reply(`**Top 10 Leaderboard**\n${leaderboardMessage}`);
 }
 
